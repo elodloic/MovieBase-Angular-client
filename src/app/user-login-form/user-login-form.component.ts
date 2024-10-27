ec28bc8f625fc277ca41b452ee5e9f9d30595883
elodloic <95284344+elodloic@users.noreply.github.com>
2024-10-27 20:33:22 +0100

    Comment updates
    Updated comments in form component files.

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,12 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-
-// You'll use this import to close the dialog on success
 import { MatDialogRef } from '@angular/material/dialog';
-
-// This import brings in the API calls we created in 6.2
 import { FetchApiDataService } from '../fetch-api-data.service';
-
-// This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 
@@ -30,9 +24,9 @@ ngOnInit(): void {
 // This is the function responsible for sending the form inputs to the backend
 loginUser(): void {
     this.FetchApiDataService.userLogin(this.loginData).subscribe((result) => {
-      localStorage.setItem("user", JSON.stringify(result.user));
-      localStorage.setItem("token", result.token);     
-      this.dialogRef.close(); // This will close the modal on success!
+      localStorage.setItem("user", JSON.stringify(result.user));  // Storing the user in the local storage
+      localStorage.setItem("token", result.token);                // Storing the token in the local storage
+      this.dialogRef.close();                                     // This will close the modal on success!
       this.snackBar.open("Login successful", 'OK', {
         duration: 2000
      });
--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,12 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-
-// You'll use this import to close the dialog on success
 import { MatDialogRef } from '@angular/material/dialog';
-
-// This import brings in the API calls we created in 6.2
 import { FetchApiDataService } from '../fetch-api-data.service';
-
-// This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 
